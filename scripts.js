dcb59e04b5dc5934640eed643d3a8310a4fae0a8
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-06-22 17:00:43 -0500

    working on highlight

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 $( document ).ready(function() {
-    $('#keyboard-upper-container').hide();
+    $('#keyboard-upper-container').css({'display':'none'});
 });
 
 $(document).keydown(function(e){
     if(e.which==16){
-        console.log('hi');
+        $('#keyboard-lower-container').css({'display':'none'});
+        $('#keyboard-upper-container').css({'display':'block'})
 }     
 })
+$(document).keyup(function(e){
+    if(e.which==16){
+        $('#keyboard-upper-container').css({'display':'none'});
+        $('#keyboard-lower-container').css({'display':'block'})
+}     
+})
+var span='span.id'
 
-
+$('.key-row').keypress(function(e){
+    if(e.keyCode==span)
+        $( span.id).toggle( "highlight" );
+        console.log('hi')
+})
