23fa332f44e98566bcc2645e4e8c2349a1c820bf
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-06-26 17:49:00 -0500

    rough rough draft

--- scripts.js
+++ scripts.js
@@ -2,28 +2,56 @@ var targetLetter=document.getElementById('target-letter')
 var words=document.getElementById('sentence');
 var block=document.getElementById('yellow-block');
 var pos=$('#yellow-block').position();
-var sentenceCounter=[0]
 var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
 'Too ato too nOt enot one totA not anot tOO aNot', 
 'oat itain oat tain nate eate tea anne inant nean',
 'itant eate anot eat nato inate eatanot tain eat', 
 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+var sentenceNum=0
+var j=sentenceNum
+var letterCount=0
+var counter=letterCount++
+
+var spaghetti=sentences[j].charAt(letterCount)
+
+
+$('yellow-block').append(sentences[sentenceNum]);
 
 $(document).ready(function(){ 
-    words.innerHTML=sentences[0].toString();
-    console.log(sentences.toString());
+    words.innerHTML=sentences[sentenceNum].toString();
 })
 
-function sentenceSelector(sentenceNum,){
-   console.log( $(sentences).array_values())
-}
-
-
-
-
-$(document).keypress(function(){
-    $('#yellow-block').animate({"left":"+=0.70em"},15);
+$(document).keypress(function(e){
+    var feedback=String.fromCharCode(e.which)
+    $('#target-letter').text(feedback)
+    if('#target-letter'==true)
+    console.log($('#feedback').addClass('.glyphicon glyphicon-ok'))
+})      
+    
+$(document).keypress(function(e){
+    console.log(spaghetti)
+    //letterCount++
+   // console.log(letterCount)
+    $('#yellow-block').animate({"left":"+=0.72em"},15);
 })
+
+    /*  if(48<=letterCount){
+        $(block).css('margin-left','-850px');
+        words.innerHTML=sentences[sentenceNum+1]
+       // letterCount++
+     } else if(97<letterCount){
+        words.innerHTML=sentences[sentenceNum+2]
+        $(block).css('margin-left','-850px');
+        letterCount++
+          }
+           else if(140<letterCount){
+        words.innerHTML=sentences[sentenceNum+2]
+        $(block).css('margin-left','-850px');
+        letterCount++
+          }
+
+
+})*/
 
 
 $( document ).ready(function() {
